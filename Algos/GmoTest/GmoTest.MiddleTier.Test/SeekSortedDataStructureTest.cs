0126819a94c38cb535099b40e3211d7da1fdd4b3
Shlomo Abraham <spamthisaddress@live.com>
2019-10-08 09:03:27 -0700

    Added SeekSortedDataStructure to VS solution. Added tests.

---
+++ Algos/GmoTest/GmoTest.MiddleTier.Test/SeekSortedDataStructureTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using GmoTest.MiddleTier;
+
+namespace GmoTest.MiddleTier.Test
+{
+    [TestClass]
+    public class SeekSortedDataStructureTest
+    {
+        private int[,] GivenMatrix => new int[,] {
+            {4, 7, 12, 23, 27, 34, },
+            {6, 10, 15, 24, 30, 40, },
+            {12, 15, 18, 29, 32, 48, },
+            {14, 18, 21, 30, 35, 54, },
+            {20, 23, 24, 35, 37, 62, },
+            {22, 27, 29, 39, 40, 68, },
+            {28, 32, 33, 44, 46, 76, },
+            {30, 36, 38, 48, 49, 82, },
+        };
+
+        [TestMethod]
+        public void Test_Given_Corners()
+        {
+            Assert.AreEqual(true, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 4));
+            Assert.AreEqual(true, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 34));
+            Assert.AreEqual(true, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 30));
+            Assert.AreEqual(true, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 82));
+        }
+
+        [TestMethod]
+        public void Test_Given_OutOfBounds()
+        {
+            Assert.AreEqual(false, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 3));
+            Assert.AreEqual(false, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 83));
+        }
+
+        [TestMethod]
+        public void Test_Given_GivenCases()
+        {
+            Assert.AreEqual(true, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 18));
+            Assert.AreEqual(false, SeekSortedDataStructureClass.SeekSortedDataStructureRec(GivenMatrix, 26));
+        }
+    }
+}
---
+++ Algos/GmoTest/GmoTest.MiddleTier/SeekSortedDataStructureClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GmoTest.MiddleTier
+{
+    public static class SeekSortedDataStructureClass
+    {
+        public static bool SeekSortedDataStructureRec(int[,] array, int numToFind)
+        {
+            var m = array.GetUpperBound(0);
+            var n = array.GetUpperBound(1);
+            if (m == 0 || n == 0)
+                return false;
+
+            return SeekSortedDataStructureRec(array, numToFind, 0);
+        }
+
+        public static bool SeekSortedDataStructureRec(int[,] array, int numToFind, int column)
+        {
+            if (column > array.GetUpperBound(0))
+                return false;
+            if (array[column, 0] > numToFind)
+                return false;
+
+            for (int i = 0; i <= array.GetUpperBound(1); i++)
+            {
+                if (array[column, i] == numToFind)
+                    return true;
+                if (array[column, i] > numToFind)
+                    return SeekSortedDataStructureRec(array, numToFind, column + 1);
+            }
+            return SeekSortedDataStructureRec(array, numToFind, column + 1);
+        }
+    }
+}
