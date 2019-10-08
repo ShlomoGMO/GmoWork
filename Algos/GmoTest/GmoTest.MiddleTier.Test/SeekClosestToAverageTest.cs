b8069558970e5cceb37af47b9e69f7752269caff
Shlomo Abraham <spamthisaddress@live.com>
2019-10-08 08:57:10 -0700

    Added SeekClosestToAverage to VS Solution. Added tests.

---
+++ Algos/GmoTest/GmoTest.MiddleTier.Test/SeekClosestToAverageTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using GmoTest.MiddleTier;
+
+namespace GmoTest.MiddleTier.Test
+{
+    [TestClass]
+    public class SeekClosestToAverageTest
+    {
+        [TestMethod]
+        public void GivenTestCase()
+        {
+            var testArray = new[] { 1, 2, 3, 5, -1, 7, 145, -33, 22, 14 };
+            var result = SeekClosestToAverageClass.SeekClosestToAverage(testArray);
+
+            Assert.AreEqual(14, result);
+        }
+
+        [TestMethod]
+        public void EmptyArray()
+        {
+            Assert.ThrowsException<Exception>(() => SeekClosestToAverageClass.SeekClosestToAverage(new int[0]));
+        }
+
+        [TestMethod]
+        public void SingleItemArray()
+        {
+            var testArray = new[] { 1 };
+            var result = SeekClosestToAverageClass.SeekClosestToAverage(testArray);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void RepeatingArray()
+        {
+            var testArray = new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, };
+            var result = SeekClosestToAverageClass.SeekClosestToAverage(testArray);
+
+            Assert.AreEqual(2, result);
+        }
+
+
+
+    }
+}
---
+++ Algos/GmoTest/GmoTest.MiddleTier/SeekClosestToAverage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GmoTest.MiddleTier
+{
+    public static class SeekClosestToAverageClass
+    {
+        public static int SeekClosestToAverage(int[] array)
+        {
+            if (array.Length == 0)
+                throw new Exception("Contents empty.");
+
+            var average = array.Average();
+            int closest = array[0];
+            double closestDiff = Math.Abs(closest - average);
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var localNum = array[i];
+                var localDiff = Math.Abs(localNum - average);
+                if (localDiff < closestDiff)
+                {
+                    closest = localNum;
+                    closestDiff = localDiff;
+                }
+            }
+            return closest;
+        }
+
+    }
+}
